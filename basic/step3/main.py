e16e955b8879c19c20550ddd22d5e28ef435b399
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-11-17 18:39:04 +0900

    fix:step2

--- basic/step3/main.py
+++ basic/step3/main.py
@@ -3,11 +3,10 @@
 # BMIの計算方法は、体重(kg)を身長(m)の2乗で割ったものです．
 
 def main():
-    # ここに書いてね
     height = 1.7
     width = 60
-
-    bmi = width / (height ** 2)
-    print(round(bmi, 1))
+    # ここに書いてね
+    bmi = ""
+    print("")
 if __name__ == '__main__':
     main()
