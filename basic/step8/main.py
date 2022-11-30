ecd95e7d37b342c2fff3bb66ba0ec741952b13f3
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-12-01 01:51:48 +0900

    add:step

--- basic/step8/main.py
+++ basic/step8/main.py
@@ -1,2 +1,14 @@
-#Step8
-#可変オブジェクトに関する問題，トラブルシューティング問題(WIP
+# Step8
+# 以下のコードは意図した通りに動作しません。動作するように修正してください。
+# welcom関数内の変更はできない物とします。
+
+def welcom(u):
+    print('ようこそ{}さん'.format(u['name']))
+    u['age'] = u['age'] + 1
+    print('あなたは来年{}歳になりますね'.format(u['age']))
+
+username = input()
+userage = int(input())
+user = {'name': username, 'age': userage}
+
+welcom(user)
--- basic/step9/main.py
+++ basic/step9/main.py
@@ -1,2 +1,28 @@
 #Step9
 #クラスを用いて，モジュールの作成（WIP
+
+# クラス名はBMIとしてください
+# インスタンス変数は次の５つを使用：name, weight, height, bmi, himando
+# 各メソッドの雛形を、引数も含め適切な処理に記述し直してください
+# コンストラクタではname, weight, height を初期化してください
+# set_bmi()メソッドではBMI値を算出してインスタンス変数 bmi にセット
+# set_himand()メソッドではBMI値を元に肥満度を決定し、インスタンス変数 himando にセット
+# 肥満度の基準値は下記の表に基づいてください
+# judge()メソッドでは下記の書式で結果を出力してください
+
+class BMI:
+    # コンストラクタ
+    def __init__():
+        pass # インスタンス変数に名前と体重と身長を初期化
+
+    def set_bmi():
+        pass # BMIを計算してインスタンス変数 bmi にセット
+
+    def set_himando():
+        pass # 肥満度をインスタンス変数 himando にセット
+
+    def judge():
+        pass # 指定の書式で情報を出力
+
+tanaka = Human("田中", 69, 172)
+tanaka.judge()
