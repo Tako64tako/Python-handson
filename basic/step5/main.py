01970337423554560433029718654d72b14cb513
wind111-lang <k20076kk@aitech.ac.jp>
2022-12-03 20:07:22 +0900

    Add: step7 Fix:step5

--- basic/step5/main.py
+++ basic/step5/main.py
@@ -2,13 +2,16 @@
 # 変数bmiから20より大きい,25より小さいか判定するプログラムを作成してください.
 # bmiを出力し，返り値をbooleanで返してください.
 
+min = 20
+max = 25
+
+
 def main():
     height = 1.7
     width = 60
     # heightとwidthは適宜変更してね
 
     # ここに書いてね
-    
 
 
 if __name__ == '__main__':
