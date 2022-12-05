ef5ccff55268f9007f3e8ce04124cc785c569519
wind111-lang <k20076kk@aitech.ac.jp>
2022-12-05 13:43:29 +0900

    Add:test_binary Fix:all test style

--- basic/step1/main.py
+++ basic/step1/main.py
@@ -3,6 +3,6 @@
 
 def main():
    #ここに書いてね
-
+  print("Hello,World!")
 if __name__ == '__main__':
    main()
--- basic/step1/main_test.py
+++ basic/step1/main_test.py
@@ -1,17 +1,13 @@
 import unittest
-from io import StringIO
-from contextlib import redirect_stdout
-
-import main
-
+import subprocess
 
 class TestCase(unittest.TestCase):
     def test_main(self):
-        io = StringIO()
-        with redirect_stdout(io):
-            main.main()
+        ret = subprocess.run(["python3","main.py"],capture_output=True,text=True)
+        #main.pyを実行して，標準出力を文字列で受け取り
+　　#ret.stdoutが標準出力の結果ß
         self.assertRegex(
-            io.getvalue(), r'[hH][eE][lL][lL][oO][,\s]*[wW][oO][rR][lL][dD][!]*\n*')
+            ret.stdout, r'[hH][eE][lL][lL][oO][,\s]*[wW][oO][rR][lL][dD][!]*\n*')
 
 if __name__ == '__main__':
     unittest.main()
