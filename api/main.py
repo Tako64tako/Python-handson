484bcf9b0ea54475c78cd7cad8465029c29247f0
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-12-08 19:24:49 +0900

    add:api

--- api/main.py
+++ api/main.py
@@ -1,19 +1,29 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    user = request.args.get('user')
-    data = request.args.get('data')
-    return jsonify({'user': user, 'data': data})
+def index():
+    return render_template('index.html')
 
-@app.route('/post', methods=['POST'])
-def post():
-    user = request.form['user']
-    data = request.form['data']
-    return jsonify({'user': user, 'data': data})
+# TODO: GETメソッドで/apiにアクセスしたときの処理を書いてください
+# return render_template('get.html')を返すようにしてください
+@app.route('/api')
+def get_articles():
+    return render_template('get.html')
+
+# TODO: POSTメソッドで/api/articlesにアクセスしたときの処理を書いてください
+# name = request.form.get("name")でフォームから送られてきたnameを取得してください
+# nameがない場合は"名無し"を代入してください
+# return render_template('post.html', name=name)を返すようにしてください
+@app.route('/api/articles', methods=['POST'])
+def get_api_articles():
+    name = request.form.get("name")
+    if not name:
+        name = "名無し"
+    return render_template('post.html', name=name)
 
 
 if __name__ == "__main__":
+    # TODO: host='127.0.0.1',port=8888,debug=Trueで起動してください
     app.run(host='127.0.0.1', port=8888, debug=True)
