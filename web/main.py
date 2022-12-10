2e875a9586c0821adc011dff13867d19f361423b
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-12-10 23:00:19 +0900

    del:回答例

--- web/main.py
+++ web/main.py
@@ -5,13 +5,10 @@ def main():
     url = "https://www.p-world.co.jp/index.html"
 
     # TODO: requests.get()でurlのHTMLを取得してください
-    r = requests.get(url)
 
     # TODO: BeautifulSoup()でHTMLを解析してください
-    soup = BeautifulSoup(r.content, "html.parser")
 
     # TODO: soup.select()でh1タグの要素を出力してください
-    print(soup.select("h1"))
 
     return soup.select("h1")
 if __name__ == "__main__":
