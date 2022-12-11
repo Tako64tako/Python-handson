a228510d535ff5b1fcb60cfc40977bac2f148f68
松田　雄飛 <k20109kk@aitech.ac.jp>
2022-12-11 09:27:12 +0900

    feat:readme

--- library/test.py
+++ library/test.py
@@ -1,9 +1,8 @@
-import pachinko
-import time
-pachinko.pachikasu()
-time.sleep(1)
-pachinko.dero()
-time.sleep(1)
-pachinko.chinkasu()
-time.sleep(1)
-pachinko.dero()
+# TODO: ライブラリをsetup.pyと同じディレクトリでインストールします.
+#       このスクリプトを実行する前に, 以下のコマンドを実行してください.
+#       $ pip install .
+
+# TODO: ライブラリをインポートします.
+
+# TODO: ライブラリを使ってみます.
+#       ライブラリの関数はpachinko.pyに定義されています.
